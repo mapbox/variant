7423b70b907b640b477c9675d79b43abb6bf21ed
Jochen Topf <jochen@topf.org>
2014-02-12 18:40:24 +0100

    Fixed var names

--- variant.hpp
+++ variant.hpp
@@ -90,7 +90,7 @@ struct variant_helper<T, Types...>
     {
         if (old_id == sizeof...(Types))
         {
-            new (new_v) T(std::move(*reinterpret_cast<T*>(old_value)));
+            new (new_value) T(std::move(*reinterpret_cast<T*>(old_value)));
             //std::memcpy(new_v, old_v, sizeof(T));
             // ^^  DANGER: this should only be considered for relocatable types e.g built-in types
             // Also, I don't see any measurable performance benefit just yet
@@ -105,7 +105,7 @@ struct variant_helper<T, Types...>
     {
         if (old_id == sizeof...(Types))
         {
-            new (new_v) T(*reinterpret_cast<const T*>(old_value));
+            new (new_value) T(*reinterpret_cast<const T*>(old_value));
         }
         else
         {
