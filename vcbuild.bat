bb12b87574fcdaa7bdca6c1529d442eab2d52b97
Dane Springmeyer <dane@mapbox.com>
2014-05-23 11:05:22 -0700

    Add vcbuild.bat

---
+++ vcbuild.bat
@@ -0,0 +1,5 @@
+git clone --depth 1 https://chromium.googlesource.com/external/gyp.git deps/gyp
+del variant.sln
+del tests.vcxproj
+C:\Python27\python.exe deps/gyp/gyp_main.py variant.gyp --depth=. -f msvs -G msvs_version=2013
+msbuild variant.sln /clp:Verbosity=minimal /nologo /p:Configuration=Release;Platform=Win32
