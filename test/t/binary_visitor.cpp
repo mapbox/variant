5d9845c5d387cc81f12857361864320e38a75d57
Jochen Topf <jochen@topf.org>
2016-01-27 21:40:04 +0100

    Try to test more in binary visitor test.

--- test/t/binary_visitor.cpp
+++ test/t/binary_visitor.cpp
@@ -6,7 +6,7 @@
 #include "variant.hpp"
 #include "variant_io.hpp"
 
-using variant_type = mapbox::util::variant<bool, int, double>;
+using variant_type = mapbox::util::variant<bool, int, double, char>;
 
 struct add_visitor
 {
