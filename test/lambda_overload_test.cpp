4d462f27b2d852f66a3769e5983691c0f9233c9e
Daniel J. Hofmann <daniel@trvx.org>
2016-11-14 12:41:35 +0100

    Adds C++14 SFINAE Test

--- test/lambda_overload_test.cpp
+++ test/lambda_overload_test.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <vector>
 
 #include <mapbox/variant.hpp>
 #include <mapbox/variant_visitor.hpp>
+
+#if __cplusplus >= 201402L
+#define HAS_CPP14_SUPPORT
+#endif
 
 using namespace mapbox::util;
 
@@ -50,9 +55,38 @@ void test_singleton_variant()
     apply_visitor(make_visitor([](int) {}), singleton);
 }
 
+#ifdef HAS_CPP14_SUPPORT
+void test_lambda_overloads_sfinae()
+{
+    variant<int, float, std::vector<int>> var;
+
+    auto visitor = make_visitor([](auto range) -> decltype(std::begin(range), void()) {
+                                    for (auto each : range)
+                                        std::cout << each << ' '; },
+                                [](auto x) -> decltype(std::cout << x, void()) {
+                                    std::cout << x << std::endl;
+                                });
+
+    var = 1;
+    apply_visitor(visitor, var);
+
+    var = 2.f;
+    apply_visitor(visitor, var);
+
+    var = std::vector<int>{4, 5, 6};
+    apply_visitor(visitor, var);
+}
+#endif
+
 int main()
 {
     test_lambda_overloads();
     test_singleton_variant();
     test_lambda_overloads_capture();
+
+#ifdef HAS_CPP14_SUPPORT
+    test_lambda_overloads_sfinae();
+#endif
 }
+
+#undef HAS_CPP14_SUPPORT
