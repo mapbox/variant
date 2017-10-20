4b98c485cf7d74691f7921145054641daa66936e
Artem Pavlenko <artem@mapnik.org>
2017-10-20 09:30:18 +0200

    add lost test check + remove stderr

--- test/t/variant_alternative.cpp
+++ test/t/variant_alternative.cpp
@@ -4,7 +4,6 @@
 #include <mapbox/variant_io.hpp>
 
 #include <string>
-#include <iostream>
 
 TEST_CASE("variant alternative", "[types]")
 {
@@ -13,11 +12,11 @@ TEST_CASE("variant alternative", "[types]")
     using type_1 = mapbox::util::variant_alternative<1, variant_type>::type;
     using type_2 = mapbox::util::variant_alternative<2, variant_type>::type;
 
-    std::cerr << typeid(type_0).name() << std::endl;
-    std::cerr << typeid(type_1).name() << std::endl;
-    std::cerr << typeid(type_2).name() << std::endl;
     constexpr bool check_0 = std::is_same<int, type_0>::value;
     constexpr bool check_1 = std::is_same<double, type_1>::value;
     constexpr bool check_2 = std::is_same<std::string, type_2>::value;
+
     CHECK(check_0);
+    CHECK(check_1);
+    CHECK(check_2);
 }
